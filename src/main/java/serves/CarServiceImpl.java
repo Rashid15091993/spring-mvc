7e86822b3a9cdccc653af263c7b7f3b8482f0e15
rashi <rashid058@mial.ru>
2021-10-25 20:07:50 +0500

    2.2.2

---
+++ src/main/java/serves/CarServiceImpl.java
@@ -0,0 +1,23 @@
+package serves;
+
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CarServiceImpl {
+    public static List<Car> carsCount(int number) {
+
+        List<Car> list = new ArrayList<>();
+        list.add(new Car("Ford", "RED", 1000));
+        list.add(new Car("Ferrari", "YELLOW", 700));
+        list.add(new Car("Aston Martin", "BLACK", 500));
+        list.add(new Car("FIAT", "GREEN", 180));
+        list.add(new Car("BMW", "BLUE", 100));
+        if (number == 0 || number > 5) {
+            return list;
+        }
+        return list.stream().limit(number).collect(Collectors.toList());
+    }
+}
--- src/main/java/web/controller/CarControler.java
+++ src/main/java/web/controller/CarControler.java
@@ -1,28 +1,18 @@
 package web.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import serves.CarService;
-import web.model.Car;
-
-import java.util.ArrayList;
-import java.util.List;
+import serves.CarServiceImpl;
 
 @Controller
 public class CarControler {
-
+    CarServiceImpl carService;
     @GetMapping(value = "/car")
     public String car(@RequestParam(value = "count", defaultValue = "5") int allCars, Model model) {
-        List<Car> list = new ArrayList<>();
-        list.add(new Car("Ford", "RED", 1000));
-        list.add(new Car("Ferrari", "YELLOW", 700));
-        list.add(new Car("Aston Martin", "BLACK", 500));
-        list.add(new Car("FIAT", "GREEN", 180));
-        list.add(new Car("BMW", "BLUE", 100));
-        list = CarService.carsCount(list, allCars);
-        model.addAttribute("list", list);
+
+        model.addAttribute("list", carService.carsCount(allCars));
         return "car";
     }
 }
